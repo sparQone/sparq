192605356fe592c9b00122e48df86398246b79e2
Asim Baig <asim95@gmail.com>
2025-01-28 14:57:07 -0600

    Minor.

--- system/module/module_manager.py
+++ system/module/module_manager.py
@@ -7,7 +7,7 @@ hookimpl = pluggy.HookimplMarker("sparqone")
 class ModuleSpecs:
     @hookspec
     def init_database(self):
-        """Initialize database tables and sample data for the module.
+        """Optional: Initialize database tables and sample data for the module.
         This hook is called after all modules are loaded and the database
         connection is established.
         """
