1d1c0406bd98640533d1fd2bf5dfa747cc2c6704
Asim Baig <asim95@gmail.com>
2025-02-12 00:01:50 -0600

    TODO: get company default language if user language is not set

--- modules/core/utils/language.py
+++ modules/core/utils/language.py
@@ -2,4 +2,7 @@ def get_user_language(user):
     """Get user's language, falling back to company default if not set"""
     if user.settings and user.settings.language:
         return user.settings.language
-    return get_company_settings().default_language 
+
+    # TODO: get company default language if user language is not set
+    # return get_company_settings().default_language 
+    return "en"
